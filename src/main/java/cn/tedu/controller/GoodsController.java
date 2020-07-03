14bb45f31da1a7c2c4bb7f18ea223cb37b7faa60
余浩 <980187223@qq.com>
2020-07-03 11:55:31 +0800

    goods.html添加"<h1></h1>"属性

--- src/main/java/cn/tedu/controller/GoodsController.java
+++ src/main/java/cn/tedu/controller/GoodsController.java
@@ -2,14 +2,18 @@ package cn.tedu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("goods")
 public class GoodsController {
-    @RequestMapping("doGoodUI")
-    @ResponseBody
+    //请求地址
+    //DispatcherServlet对象基于用户输入的url找到对应的Controller对象
+    //DispatcherServlet底层会通过反射技术调用相对应的控制层方法
+    @RequestMapping("doGoodsUI")
     public String doGoodUI(){
-        return "goods";
+        return "goods";//view name
+        //1.此view返回给前端控制器(DispatcherServlet)
+        //2.前端控制器会调用视图解析器对view进行解析，添加前缀和后缀
+        //3.最后由DispatcherServlet将页面响应到客户端
     }
 }
