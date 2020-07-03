5b5306b55bd5a68a6b8e22ba160b81e1ba041b66
余浩 <980187223@qq.com>
2020-07-03 17:21:32 +0800

    goods.html添加删除接口+自定义日期格式“yyyy/MM/dd HH:mm”

--- src/main/java/cn/tedu/controller/GoodsController.java
+++ src/main/java/cn/tedu/controller/GoodsController.java
@@ -14,19 +14,26 @@ import java.util.List;
 public class GoodsController {//web处理层
     @Autowired
     private GoodsService goodsService;
+
     //请求地址
     //DispatcherServlet对象基于用户输入的url找到对应的Controller对象
     //DispatcherServlet底层会通过反射技术调用相对应的控制层方法
     @RequestMapping("doGoodsUI")
-    public String doGoodUI(Model model){
+    public String doGoodsUI(Model model){
         //获取业务数据
         List<Goods> list = goodsService.findObjects();
         //将数据存储到作用域对象
         model.addAttribute("list",list);
         //将页面响应到客户端
+
         return "goods";//view name
         //1.此view返回给前端控制器(DispatcherServlet)
         //2.前端控制器会调用视图解析器对view进行解析，添加前缀和后缀
         //3.最后由DispatcherServlet将页面响应到客户端
+    }
+    @RequestMapping("doDeleteById")
+    public String doDeleteById(Integer id){
+        goodsService.deleteById(id);
+        return "redirect:doGoodsUI";
     }
 }
